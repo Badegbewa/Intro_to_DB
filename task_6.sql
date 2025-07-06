3c701bbdb85703dcb0d74e19d23039468a37c5b5
Adegbewa Beyioku <beyiokuenny@gmail.com>
2025-07-07 00:23:01 +0100

    added multiple customer id to column

--- task_6.sql
+++ task_6.sql
@@ -1,1 +1,7 @@
+USE alx_book_store;
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES 
+(5, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(6, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(7, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
 
