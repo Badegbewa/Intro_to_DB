aa582d5ea6dc36d9fc63f57a3de78886d3e38203
Adegbewa Beyioku <beyiokuenny@gmail.com>
2025-07-07 00:14:45 +0100

    made changes

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,5 @@
 USE alx_book_store;
-INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+INSERT INTO customers (customer_id, customer_name, email, address)
 VALUES (2, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 
 
