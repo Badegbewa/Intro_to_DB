6ec450ad8fc140a0bc334116df18fe9fb540d696
Adegbewa Beyioku <beyiokuenny@gmail.com>
2025-07-07 01:22:41 +0100

    made changes to meet checker

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector
-
 from mysql.connector import Error
 
 def create_database():
@@ -9,20 +8,21 @@ def create_database():
             user='root',
             password='@Father20'
         )
-
+        
         if connection.is_connected():
             cursor = connection.cursor()
-            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
-            
-    except Error as e:
-        print(f"Error: {e}")
     
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+
     finally:
         if connection.is_connected():
             cursor.close()
             connection.close()
-            print("MySQL connection closed.")
+            print("MySQL connection is closed.")
 
 if __name__ == "__main__":
     create_database()
+
